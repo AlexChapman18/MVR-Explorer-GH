5f742668d94136edf22c52ef6573e5ebb6e2bc4c
Alex Chapman <64100075+AlexChapman18@users.noreply.github.com>
2025-01-16 01:11:54 +0000

    Implemented ESLint and Prettier

--- src/utils/matrixUtils.ts
+++ src/utils/matrixUtils.ts
@@ -1,15 +1,28 @@
 import { Matrix4 } from "three";
 
 export function parseMatrix(matrixString: string): Matrix4 {
-    const matrix = matrixString
-        .slice(1, -1) // Remove the outer curly braces
-        .replaceAll('}{', ',') // Im lazy, sorry :(
-        .split(',').map(parseFloat);
+  const matrix = matrixString
+    .slice(1, -1) // Remove the outer curly braces
+    .replace(/}{/g, ",") // Im lazy, sorry :(
+    .split(",")
+    .map(parseFloat);
 
-    return new Matrix4(
-        matrix[0], matrix[3], matrix[6], matrix[9],
-        matrix[1], matrix[4], matrix[7], matrix[10],
-        matrix[2], matrix[5], matrix[8], matrix[11],
-        0, 0, 0, 1
-    );
+  return new Matrix4(
+    matrix[0],
+    matrix[3],
+    matrix[6],
+    matrix[9],
+    matrix[1],
+    matrix[4],
+    matrix[7],
+    matrix[10],
+    matrix[2],
+    matrix[5],
+    matrix[8],
+    matrix[11],
+    0,
+    0,
+    0,
+    1,
+  );
 }
