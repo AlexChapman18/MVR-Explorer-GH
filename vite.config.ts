1681564016f758ed2e795edd0c549c75dbfa1a63
Alex Chapman <alex.d.chapman@outlook.com>
2025-01-23 20:12:49 +0000

    Possible fix for build issue

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
-  base: "/MVR-Explorer/",
+    plugins: [react()],
+    base: "/MVR-Explorer/",
 });
