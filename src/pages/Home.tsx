04f3a2b5cf189018812c302b7b8fed1272499efd
Alex Chapman <alex.d.chapman@outlook.com>
2025-01-28 21:46:39 +0000

    Not sure where i am now

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,19 @@
 import MVRContainer from "../components/MVRContainer.tsx";
 import LoadingModal from "../components/LoadingModal.tsx";
 import { ProgressStepsProvider } from "../contexts/ProgressContext.tsx";
+import { MVRFileProvider } from "../contexts/MVRFileContext.tsx";
+import FileUpload from "../components/FileUpload.tsx";
 
 function Home() {
     return (
-        <ProgressStepsProvider>
-            <h1 className="text-3xl font-bold underline"> Hello world! </h1>
-            <LoadingModal />
-            <MVRContainer />
-        </ProgressStepsProvider>
+        <MVRFileProvider>
+            <ProgressStepsProvider>
+                <h1 className="text-3xl font-bold underline"> Hello world! </h1>
+                <FileUpload />
+                <LoadingModal />
+                <MVRContainer />
+            </ProgressStepsProvider>
+        </MVRFileProvider>
     );
 }
 
